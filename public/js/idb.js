0f0adee0364a3ca8f0c5d5cfc7833797a5ff37ea
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-07-01 18:56:34 -0400

    upload function for offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,17 +6,55 @@ request.onupgradeneeded = function (event) {
     // save reference to database 
     const db = event.target.result;
     db.createObjectStore('new_budget', { autoIncrement: true });
-  };
+    };
 
 request.onsuccess = function(event) {
 // if db was created successfully with object store, save to global variable
     db = event.target.result;
+     // if online, upload any pending records
+    if (navigator.onLine) {
+        uploadBudget();
+        }
     };
 
 request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
-  };
+    };
+  
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObjectStore.getAll();
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+    // if there was data in indexedDB's store, send it to the API server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            // if saving to server worked, clear pending records
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                budgetObjectStore.clear();
+                alert('Your saved budgets has been submitted!');
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        }
+    };
+};
 
 // function to be executed if new budget is created offline
 function saveRecord(record) {
@@ -27,4 +65,7 @@ function saveRecord(record) {
     // add record to the store
     budgetObjectStore.add(record);
   };
+
+  // listen for online status
+  window.addEventListener('online', uploadBudget);
 
