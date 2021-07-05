9d68aa4553349028729693a02df3303ddd915563
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-07-04 22:27:13 -0400

    bug fix

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
+
 
 // routes
 app.use(require("./routes/api.js"));
